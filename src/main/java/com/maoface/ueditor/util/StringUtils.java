9b707d281d7cedd98e3929ab19eaed5cd5562018
zhuxc <xuchao.zhu@outlook.com>
2023-01-27 15:15:31 +0800

    字符串比较工具方法

--- src/main/java/com/maoface/ueditor/util/StringUtils.java
+++ src/main/java/com/maoface/ueditor/util/StringUtils.java
@@ -14,4 +14,9 @@ public class StringUtils {
     public static boolean isEmpty(CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
+
+    public static boolean anyEquals(String str, String... others) {
+        // TODO: 2023/1/27 需要实现给定字符串满足equals字符串数组任一元素的逻辑
+        return false;
+    }
 }
