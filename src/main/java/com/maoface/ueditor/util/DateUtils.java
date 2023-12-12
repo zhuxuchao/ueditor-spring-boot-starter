bd196a32c1ae5cbd7b72666c1cdedc46a0a9df3f
zhuxc <xuchao.zhu@outlook.com>
2023-12-12 14:37:41 +0800

    新增DateUtils工具类处理各种日期操作

---
+++ src/main/java/com/maoface/ueditor/util/DateUtils.java
@@ -0,0 +1,41 @@
+package com.maoface.ueditor.util;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.StrUtil;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * 日期类型工具类
+ * @author zhuxc
+ * @since 1.0
+ */
+public abstract class DateUtils {
+    public static final String NORM_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static String format(Date date){
+        return format(date, NORM_DATETIME_PATTERN);
+    }
+
+    public static String format(Date date, String format) {
+        if (null != date && !StrUtil.isBlank(format)) {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            if (date instanceof DateTime) {
+                TimeZone timeZone = ((DateTime) date).getTimeZone();
+                if (null != timeZone) {
+                    sdf.setTimeZone(timeZone);
+                }
+            }
+
+            return format(date, sdf);
+        } else {
+            return null;
+        }
+    }
+
+    private static String format(Date date, DateFormat format) {
+        return null != format && null != date ? format.format(date) : null;
+    }
+}
